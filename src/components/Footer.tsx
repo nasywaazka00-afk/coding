c43a83c265b0f32e9093d344739a327ca6de1579
nasywa azka <nasywaazka00@gmail.com>
2026-03-18 01:00:11 +0700

    ngantok

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   const socialLinks = [
-    { icon: Github, href: '#', label: 'GitHub' },
+    { icon: Github, href: 'https://github.com/nasywaazka00-afk/coding', label: 'GitHub' },
     { icon: Linkedin, href: '#', label: 'LinkedIn' },
     { icon: Youtube, href: '#', label: 'YouTube' },
-    { icon: Instagram, href: '#', label: 'Instagram' },
+    { icon: Instagram, href: 'https://www.instagram.com/nsywazka_n/', label: 'Instagram' },
   ];
 
   return (
