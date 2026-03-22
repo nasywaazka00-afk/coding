839169a6745014b3c3fb74d4d350b65319c69f2e
nasywa azka <nasywaazka00@gmail.com>
2026-03-22 23:05:45 +0700

    ihfuihaifh

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -34,7 +34,7 @@ export default function HeroSection() {
                 src="Nasywa Pic.jpg" 
                 alt="Nasywa Azka"
                 className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
-              />
+                />
             </div>
 
             {/* Floating Badge */}
